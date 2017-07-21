651c057eabe0e7352ace66e9684f597e50666ec6
Terasaki Shun <t.tera1217@gmail.com>
2017-07-21 16:22:14 +0900

    check free occurance

--- main.cc
+++ main.cc
@@ -4,31 +4,14 @@
 
 
 int main(){
-  variable s("s");
-  variable z("z");
-  abstraction e1("z",&z);
-  abstraction zero("s",&e1);
-
-  variable n("n");
-  application e2(&n,&s);
-  application e3(&e2,&z);
-  application e4(&s,&e3);
-  abstraction e5("z",&e4);
-  abstraction e6("s",&e5);
-  abstraction succ("n",&e6);
-  application succzero(&succ,&zero);
-  expression *exp = &succzero;
-  std::cout << exp->to_string() << std::endl;
-  char c;
-  while(std::cin>>c&&c=='s'){
-    exp = simplify(exp);
-    std::cout << exp->to_string() << std::endl;
-  }
-    
-  std::string str;
-  parser p;
-  while(std::cin >> str){
-    p(str);
-  }
+  variable x("x");
+  std::cout << x.to_string() << std::endl;
+  std::cout << free_occurance(&x,"x") << std::endl;
+  abstraction e1("x",&x);
+  std::cout << e1.to_string() << std::endl;
+  std::cout << free_occurance(&e1,"x") << std::endl;
+  application e2(&x,&e1);
+  std::cout << e2.to_string() << std::endl;
+  std::cout << free_occurance(&e2,"x") << std::endl;
   return 0;
 }
