d505831c922f5fba51346c0fdfdf67e22dd91455
Terasaki Shun <t.tera1217@gmail.com>
2017-07-21 11:40:04 +0900

    add simplify and substitute

--- expression.cc
+++ expression.cc
@@ -1,1 +1,59 @@
 #include "./expression.h"
+
+expression* simplify(expression* exp){
+  switch(exp->type){
+    case expression::VAR:
+      return exp;
+      break;
+    case expression::ABST:
+      {
+        auto abst = dynamic_cast<abstraction*>(exp);
+        return new abstraction(abst->var,simplify(abst->M));
+        break;
+      }
+    case expression::APP:
+      {
+        auto app = dynamic_cast<application*>(exp);
+        if(app->M1->type==expression::ABST){
+          auto abst = dynamic_cast<abstraction*>(app->M1);
+          return substitute(abst->M,abst->var,app->M2);
+        }
+        break;
+      }
+    default:
+      std::cerr << "something wrong!" << std::endl;
+      std::abort();
+      break;
+  }
+}
+
+expression* substitute(expression* exp,const std::string &varname,expression* exp2){
+  switch(exp->type){
+    case expression::VAR:
+      {
+        auto var = dynamic_cast<variable*>(exp);
+        if(var -> var == varname){
+          return exp2;
+        }else{
+          return var;
+        }
+        break;
+      }
+    case expression::ABST:
+      {
+        auto abst = dynamic_cast<abstraction*>(exp);
+        return new abstraction(abst->var,substitute(abst->M,varname,exp2));
+      }
+      break;
+    case expression::APP:
+      {
+        auto app = dynamic_cast<application*>(exp);
+        return new application(substitute(app->M1,varname,exp2),substitute(app->M2,varname,exp2));
+        break;
+      }
+    default:
+      std::cerr << "something wrong!" << std::endl;
+      std::abort();
+      break;
+  }
+}
--- main.cc
+++ main.cc
@@ -7,9 +7,12 @@ int main(){
   variable x("x");
   variable y("y");
   abstraction f("x",&x);
-  application g(&f,&f);
+  application g(&f,&y);
   
   std::cout << g.to_string() << std::endl;
+  auto simplified_g = simplify(&g);
+  std::cout << simplified_g->to_string() << std::endl;
+    
   std::string str;
   parser p;
   while(std::cin >> str){
